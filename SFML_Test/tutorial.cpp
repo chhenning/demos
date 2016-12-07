1d1b31f11b7a64f09de118c15e67bfa7346b8eaa
Christian Henning <chhenning@sensentia.com>
2016-12-06 21:44:50 -0500

    star field

---
+++ SFML_Test/tutorial.cpp
@@ -0,0 +1,106 @@
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+void offscreen_draw()
+{
+    sf::RenderTexture t;
+    if (t.create(400, 500) == false)
+    {
+        // error
+    }
+
+    t.clear();
+
+
+    //t.draw();
+
+    t.display();
+
+    const sf::Texture tex = t.getTexture();
+
+    sf::Sprite s(tex);
+
+    //window.draw(s);
+
+}
+
+
+
+int main()
+{
+    // Multiple windows are possible but each window event loop must be in the same thread in which the window was created.
+    // * either the same thread
+    // * each window has its own thread
+
+    // In Windows a window shouldn't be bigger than the desktop
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Star Field");
+    window.setPosition(sf::Vector2i(10, 50));
+
+    sf::Vector2u size = window.getSize();
+    unsigned int width = size.x;
+    unsigned int height = size.y;
+
+    // !!! never use together !!!
+    window.setVerticalSyncEnabled(true);
+    //window.setFramerateLimit(1);
+
+
+    sf::RectangleShape shape(sf::Vector2f(10, 100));
+    shape.setFillColor(sf::Color(100, 88, 231));
+    shape.setPosition(sf::Vector2f(200, 100));
+    shape.setOrigin(5, 50);
+
+
+    // run the main loop
+    // must be in the same thread in which a window is created
+    while (window.isOpen())
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+            {
+                window.close();
+
+                break;
+            }
+
+            case sf::Event::KeyPressed:
+            {
+                window.close();
+
+                break;
+            }
+            }
+        }
+
+
+        sf::Vector2i mouse = sf::Mouse::getPosition(window);
+        sf::Vector2f coords = window.mapPixelToCoords(mouse);
+
+
+
+        // draw it
+        window.clear();
+
+        shape.rotate(1);
+        //shape.setRotation(45);
+
+
+
+        window.draw(shape);
+
+        // copy hidden buffer into window
+        window.display();
+    }
+
+
+
+    return 0;
+}
--- Starfield/main.cpp
+++ Starfield/main.cpp
@@ -1,58 +1,152 @@
 #include <SFML/Graphics.hpp>
 
+#include <cassert>
 #include <iostream>
 
 using namespace std;
 
-void offscreen_draw()
+const int view_width = 640;
+const int view_height = 640;
+
+namespace processing
 {
-    sf::RenderTexture t;
-    if (t.create(400, 500) == false)
+    float map(float value, float start1, float stop1, float start2, float stop2)
     {
-        // error
+        assert(stop1>start1);
+        assert(stop2>start2);
+        float x = value / ( stop1 - start1 );
+
+        return start2 + ( stop2 - start2 ) * x;
     }
 
-    t.clear();
-    
-    
-    //t.draw();
-    
-    t.display();
+    float random(float low, float high)
+    {
+        assert(high>low);
 
-    const sf::Texture tex = t.getTexture();
+        return (std::rand() % ((int) high - (int) low)) + low;
+    }
 
-    sf::Sprite s(tex);
-
-    //window.draw(s);
+    float random(float high)
+    {
+        return (std::rand() % ((int)high));
+    }
 
 }
 
+struct Star
+{
+    Star() {}
 
+    Star(const int width, const int height)
+    : _width(width)
+    , _height(height)
+    {
+        _x = processing::random(-_width, _width);
+        _y = processing::random(-_height, _height);
+        _z = processing::random(_width);
+    }
+
+    void update()
+    {
+        if(_z < 1)
+        { 
+            _x = processing::random(-_width, _width);
+            _y = processing::random(-_height, _height);
+            _z = _width;
+        }
+        else
+        {
+            _z = _z - 1;
+        }
+    }
+
+    void draw(sf::RenderTarget& target) const
+    {
+        float sx = processing::map(_x / _z, -1.f, 1.f, 0.f, view_width / 2);
+        float sy = processing::map(_y / _z, -1.f, 1.f, 0.f, view_height / 2);
+
+
+        sf::CircleShape circle(3);
+        circle.setFillColor(sf::Color(255, 255, 255));
+        circle.setPosition(sf::Vector2f(sx, sy));
+
+        circle.move(sf::Vector2f(view_width/2, view_height/2));
+
+        auto pos = circle.getPosition();
+
+
+        target.draw(circle);
+    }
+
+    private:
+    
+    float _x, _y, _z;
+    int _width, _height;
+};
+
+
+
+class StarField : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    StarField(const int count)
+    {
+        _stars = std::vector<Star>(count);
+
+        for (int i = 0; i < _stars.size(); ++i)
+        {
+            _stars[i] = Star(view_width, view_height);
+        }
+
+    }
+
+    void update(sf::Time elapsed)
+    {
+        for (int i = 0; i < _stars.size(); ++i)
+        {
+            Star& s = _stars.at(i);
+
+            s.update();
+        }
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        target.clear();
+        
+        // apply the transform
+        states.transform *= getTransform();
+
+        // our particles don't use a texture
+        states.texture = NULL;
+
+        for (int i = 0; i < _stars.size(); ++i)
+        {
+            const Star& s = _stars.at(i);
+            s.draw(target);
+        }
+    }
+
+private:
+
+    std::vector<Star> _stars;
+};
 
 int main()
 {
-    // Multiple windows are possible but each window event loop must be in the same thread in which the window was created.
-    // * either the same thread
-    // * each window has its own thread
+    //srand(time(NULL));
+    srand(100);
 
-    // In Windows a window shouldn't be bigger than the desktop
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Star Field");
-    window.setPosition(sf::Vector2i(10, 50));
+    sf::RenderWindow window(sf::VideoMode(view_width, view_height), "Star Field");
 
-    sf::Vector2u size = window.getSize();
-    unsigned int width = size.x;
-    unsigned int height = size.y;
-
-    // !!! never use together !!!
     window.setVerticalSyncEnabled(true);
-    //window.setFramerateLimit(1);
 
+    StarField stars(100);
 
-    sf::RectangleShape shape(sf::Vector2f(10, 100));
-    shape.setFillColor(sf::Color(100, 88, 231));
-    shape.setPosition(sf::Vector2f(200, 100));
-    shape.setOrigin(5,50);
-
+    sf::Clock clock;
 
     // run the main loop
     // must be in the same thread in which a window is created
@@ -80,27 +174,15 @@ int main()
             }
         }
 
-        
-        sf::Vector2i mouse = sf::Mouse::getPosition(window);
-        sf::Vector2f coords = window.mapPixelToCoords(mouse);
-
-
-
-        // draw it
         window.clear();
         
-        shape.rotate(1);
-        //shape.setRotation(45);
+        sf::Time elapsed = clock.restart();
+        stars.update(elapsed);
 
+        window.draw(stars);
 
-
-        window.draw(shape);
-        
-        // copy hidden buffer into window
         window.display();
     }
-
-
 
     return 0;
 }
